3b735b5d99bf6076f852822a4e63039eb3972b06
Mahmoud Natour <71147445+Mahmoud-ElNatour@users.noreply.github.com>
2025-06-14 02:25:32 +0300

    Add files via upload

--- RentACar.Infrastructure/obj/Debug/net8.0/RentACar.Infrastructure.AssemblyInfo.cs
+++ RentACar.Infrastructure/obj/Debug/net8.0/RentACar.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RentACar.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e56ab18f72a55f63522b3c9640b23bd7bc470c26")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("RentACar.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RentACar.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
