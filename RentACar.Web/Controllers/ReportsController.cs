60ae36edfa1f31bb65eb68beff8581b1a2800856
Mahmoud Natour <71147445+Mahmoud-ElNatour@users.noreply.github.com>
2025-06-17 00:58:56 +0300

    Update navbar design and add reports placeholder

---
+++ RentACar.Web/Controllers/ReportsController.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RentACar.Web.Controllers
+{
+    [Authorize(Roles = "Admin,Employee")]
+    public class ReportsController : Controller
+    {
+        [HttpGet("/Reports")]
+        [ApiExplorerSettings(IgnoreApi = true)]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
