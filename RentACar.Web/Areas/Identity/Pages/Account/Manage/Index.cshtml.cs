bdcdc734bae3d1edfe95b1c0506607327165137b
Mahmoud Natour <71147445+Mahmoud-ElNatour@users.noreply.github.com>
2025-06-17 14:04:52 +0300

    Enhance profile management pages

--- RentACar.Web/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
+++ RentACar.Web/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using RentACar.Application.Managers;
 
 namespace RentACar.Web.Areas.Identity.Pages.Account.Manage
 {
@@ -16,13 +17,16 @@ namespace RentACar.Web.Areas.Identity.Pages.Account.Manage
     {
         private readonly UserManager<IdentityUser> _userManager;
         private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly CustomerManager _customerManager;
 
         public IndexModel(
             UserManager<IdentityUser> userManager,
-            SignInManager<IdentityUser> signInManager)
+            SignInManager<IdentityUser> signInManager,
+            CustomerManager customerManager)
         {
             _userManager = userManager;
             _signInManager = signInManager;
+            _customerManager = customerManager;
         }
 
         /// <summary>
@@ -51,10 +55,17 @@ namespace RentACar.Web.Areas.Identity.Pages.Account.Manage
         /// </summary>
         public class InputModel
         {
-            /// <summary>
-            ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-            ///     directly from your code. This API may change or be removed in future releases.
-            /// </summary>
+            [Required]
+            [Display(Name = "Full Name")]
+            public string Name { get; set; }
+
+            [Required]
+            public string Address { get; set; }
+
+            [Required]
+            [EmailAddress]
+            public string Email { get; set; }
+
             [Phone]
             [Display(Name = "Phone number")]
             public string PhoneNumber { get; set; }
@@ -64,11 +75,17 @@ namespace RentACar.Web.Areas.Identity.Pages.Account.Manage
         {
             var userName = await _userManager.GetUserNameAsync(user);
             var phoneNumber = await _userManager.GetPhoneNumberAsync(user);
+            var email = await _userManager.GetEmailAsync(user);
+
+            var customer = await _customerManager.GetCustomerByUsername(userName);
 
             Username = userName;
 
             Input = new InputModel
             {
+                Name = customer?.Name,
+                Address = customer?.Address,
+                Email = email,
                 PhoneNumber = phoneNumber
             };
         }
@@ -93,10 +110,22 @@ namespace RentACar.Web.Areas.Identity.Pages.Account.Manage
                 return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
             }
 
+            var customer = await _customerManager.GetCustomerByUsername(user.UserName);
+
             if (!ModelState.IsValid)
             {
                 await LoadAsync(user);
                 return Page();
+            }
+
+            if (Input.Email != user.Email)
+            {
+                var setEmail = await _userManager.SetEmailAsync(user, Input.Email);
+                if (!setEmail.Succeeded)
+                {
+                    StatusMessage = "Unexpected error when trying to set email.";
+                    return RedirectToPage();
+                }
             }
 
             var phoneNumber = await _userManager.GetPhoneNumberAsync(user);
@@ -108,6 +137,14 @@ namespace RentACar.Web.Areas.Identity.Pages.Account.Manage
                     StatusMessage = "Unexpected error when trying to set phone number.";
                     return RedirectToPage();
                 }
+            }
+
+            if (customer != null)
+            {
+                if (Input.Name != customer.Name)
+                    await _customerManager.UpdateCustomerName(customer.UserId, Input.Name);
+                if (Input.Address != customer.Address)
+                    await _customerManager.UpdateCustomerAddress(customer.UserId, Input.Address);
             }
 
             await _signInManager.RefreshSignInAsync(user);
