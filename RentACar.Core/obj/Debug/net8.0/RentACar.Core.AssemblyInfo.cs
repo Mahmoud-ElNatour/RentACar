d7364edb907f27b501cdf59faabc2918ddbc893e
Mahmoud Natour <71147445+Mahmoud-ElNatour@users.noreply.github.com>
2025-06-17 13:27:19 +0300

    Add files via upload

--- RentACar.Core/obj/Debug/net8.0/RentACar.Core.AssemblyInfo.cs
+++ RentACar.Core/obj/Debug/net8.0/RentACar.Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RentACar.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91aa859c075068ac52f1881d82893734d5238287")]
 [assembly: System.Reflection.AssemblyProductAttribute("RentACar.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RentACar.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
