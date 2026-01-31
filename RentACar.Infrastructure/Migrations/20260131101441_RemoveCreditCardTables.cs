da76047b9d594368b76dae7da899129b78fa2755
Mohamad Al Ayoubi <127571560+mohammadayyoubi@users.noreply.github.com>
2026-01-31 12:31:36 +0200

    migration

---
+++ RentACar.Infrastructure/Migrations/20260131101441_RemoveCreditCardTables.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RentACar.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveCreditCardTables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // Drop the CustomerCreditCard join table first (has FK to CreditCard)
+            migrationBuilder.DropTable(
+                name: "CustomerCreditCard");
+
+            // Drop the CreditCard table
+            migrationBuilder.DropTable(
+                name: "CreditCard");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            // We don't want to recreate these tables - credit card functionality is permanently removed
+            // If needed, a manual restore from backup would be required
+        }
+    }
+}
